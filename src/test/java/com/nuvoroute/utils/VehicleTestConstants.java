50c8d72c2d6488ed011143289e8ba1f3cac90ba2
sregucki <sebastian.regucki@outlook.com>
2023-10-05 19:41:06 +0200

    testing utilties

---
+++ src/test/java/com/nuvoroute/utils/VehicleTestConstants.java
@@ -0,0 +1,11 @@
+package com.nuvoroute.utils;
+
+public interface VehicleTestConstants {
+    String UUID = "df6bc480-94ef-43c5-858c-e0a2d47282a2";
+    String BRAND = "Volvo";
+    String MODEL = "FM330";
+    Integer YEAR = 2012;
+    String VIN = "1FADP3F2XCL";
+    String LICENSE_PLATE = "ABC123";
+    Integer DISTANCE_COVERED = 100000;
+}
